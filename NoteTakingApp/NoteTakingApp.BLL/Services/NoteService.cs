9029fd6b7edf7a8c1bc9df8894f6adbfa9dcd87d
iatsrangalov20 <iatsrangalov20@codingburgas.bg>
2023-10-25 22:12:21 +0300

    Add GetNote to BLL

--- NoteTakingApp/NoteTakingApp.BLL/Services/NoteService.cs
+++ NoteTakingApp/NoteTakingApp.BLL/Services/NoteService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SimpleLoginSystem.DAL.Repositories;
+using SimpleLoginSystem.DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,29 @@ using System.Threading.Tasks;
 
 namespace NoteTakingApp.BLL.Services
 {
-    internal class NoteService
+    public class NoteService
     {
+        private static NoteService instance = null;
+        private static NoteRepository repositoryInstance = NoteRepository.GetInstance();
+
+        public static NoteService GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new NoteService();
+            }
+
+            return instance;
+        }
+
+        private NoteService()
+        {
+
+        }
+
+        public List<Note> GetNotes()
+        {
+            return repositoryInstance.GetNotes();
+        }
     }
 }
