80bb9cfe56192ce89795d00b76dc2d5b9cfb133d
HHChandar20 <HHChandar20@codingburgas.bg>
2023-10-25 21:15:32 +0300

    Implement Note specifications

--- NoteTakingApp/NoteTakingApp.DAL/NoteTakingApp.DAL/Models/Note.cs
+++ NoteTakingApp/NoteTakingApp.DAL/NoteTakingApp.DAL/Models/Note.cs
@@ -8,5 +8,22 @@ namespace NoteTakingApp.DAL.Models
 {
     public class Note
     {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime LastModified { get; set; }
+
+
+        Note()
+        {
+            Id = 0;
+            Title = "Null";
+            Description = "Null";
+            LastModified = DateTime.Now;
+        }
+
     }
 }
